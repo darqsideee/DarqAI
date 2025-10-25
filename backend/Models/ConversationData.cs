426e805c51b53e4e79316c4b1cea66da09f6c330
darqsideee <darqsideee@gmail.com>
2025-10-25 15:32:57 +0200

    Create ConversationData.cs

---
+++ backend/Models/ConversationData.cs
@@ -0,0 +1,9 @@
+namespace DarqAIBackend.Models
+{
+    public class ConversationData
+    {
+        public string Query { get; set; }
+        public string Reply { get; set; }
+        public string Language { get; set; }
+    }
+}
